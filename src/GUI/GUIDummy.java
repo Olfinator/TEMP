40078476cc0b8818d1e8379270ebd96345fe8e9a
Simon Roosen <smnroosen@googlemail.com>
2013-01-25 13:48:26 +0100

    Added Dummys for Interface-implementation. These must not be modified. These may be copied into another class for testing-purposes.

---
+++ src/GUI/GUIDummy.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import Logic.LogicGUIMessageType;
+
+/*
+ * Dummy for GUI-Element. For Test-Purposes of Interaction with the other
+ * Elements, this Class may be copied and modified. This class may not be 
+ * modified directly!
+ */
+
+public class GUIDummy implements IGUI{
+	
+	public GUIDummy(boolean debug) {
+		
+	}
+	
+	@Override
+	public void ReceiceMessage(LogicGUIMessageType message, Object[] args) {
+		
+	}
+
+}
---
+++ src/Logic/DummyLogic.java
@@ -0,0 +1,25 @@
+package Logic;
+
+import GUI.GUIMessageType;
+import Network.NetworkMessageType;
+
+/*
+ * Dummy for Logic-Element. For Test-Purposes of Interaction with the other
+ * Elements, this Class may be copied and modified. This class may not be 
+ * modified directly!
+ */
+public class DummyLogic implements ILogic{
+	public DummyLogic(boolean debug) {
+		
+	}
+	
+	@Override
+	public void ReceiveMessage(GUIMessageType messageType, Object[] args) {
+		
+	}
+
+	@Override
+	public void ReceiveMessage(NetworkMessageType messageType, Object[] args) {
+		
+	}
+}
